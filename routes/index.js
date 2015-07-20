9c31da98c1a52c7785bed0eb83726d3ede352da5
MichaelHuy <lehuydit@gmail.com>
2015-07-20 22:16:04 +0700

    kid channels playlist

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,17 @@
   var express = require('express');
   var router = express.Router();
   var mongojs = require('mongojs');
-  var db = mongojs('mongodb://admin:admin123@ds059938.mongolab.com:59938/funforkid_db', ['carplaylist','movieplaylist','favoritevideo']);
+  var db = mongojs('mongodb://admin:admin123@ds059938.mongolab.com:59938/funforkid_db', ['kidchannelsplaylist','carplaylist','movieplaylist','favorites']);
 
   /* GET home page. */
   router.get('/', function(req, res) {
     res.render('index');
+  });
+
+  router.get('/api/kidchannelsplaylist', function(req, res) {
+    db.kidchannelsplaylist.find(function(err, data) {
+      res.json(data);
+    });
   });
 
   router.get('/api/carplaylist', function(req, res) {
@@ -21,6 +27,31 @@
     db.movieplaylist.find(function(err, data) {
       res.json(data);
     });
+  });
+
+
+
+
+//favorite update
+  router.get('/api/favorites', function(req, res) {
+    db.favorites.find(function(err, data) {
+      res.json(data);
+    });
+  });
+
+  router.post('/api/favorites', function(req, res) {
+    db.favorites.insert(req.body, function(err, data) {
+      res.json(data);
+    });
+
+  });
+  router.delete('/api/favorites/:_id', function(req, res) {
+    db.favorites.remove({
+      _id: mongojs.ObjectId(req.params._id)
+    }, '', function(err, data) {
+      res.json(data);
+    });
+
   });
 
   module.exports = router;
